d4d4d9aaa53209b3855d02c60afc302327d5d626
a.safronov <a.safronov@propellerads.net>
2024-02-04 19:09:10 +0100

    add test CheckingDateOfFutureEventsTest

--- src/main/java/ru/otus/pages/EventsPage.java
+++ src/main/java/ru/otus/pages/EventsPage.java
@@ -9,7 +9,12 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.MonthDay;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.List;
+import java.util.Locale;
 
 public class EventsPage {
     private final WebDriver driver;
@@ -22,6 +27,8 @@ public class EventsPage {
     private static final By FILTER_OPEN_WEBINAR =
             By.xpath(".//div[@class='dod_new-events__header-left']//a[text()='Открытый вебинар']");
     private static final By TYPE_CARDS_ON_PAGE = By.xpath(".//div[@class='dod_new-type__text']");
+    private static final By DATE_EVENTS =
+            By.xpath(".//span[contains(@class, 'time-item')][.//span[contains(@class, 'calendar-icon')]]");
 
     @Step("Выбор фильтра Открытый вебинар")
     public EventsPage selectWebinarOpeningFilter() {
@@ -39,6 +46,32 @@ public class EventsPage {
         for (WebElement card : typeCards) {
             Assertions.assertEquals("Открытый вебинар", card.getText().trim(),
                     "Один из типов карточек не соответствует 'Открытый вебинар'");
+        }
+        return this;
+    }
+
+    @Step("Дата проведения мероприятий больше или равна текущей")
+    public EventsPage eventVerificationDateGreaterOrEqualCurrentOne() {
+        List<WebElement> dateElements = driver.findElements(DATE_EVENTS);
+        MonthDay currentMonthDay = MonthDay.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d MMMM", new Locale("ru"));
+
+        System.out.println("Текущая дата (день и месяц): " + currentMonthDay);
+
+        for (WebElement dateElement : dateElements) {
+            String dateText = dateElement.getText().trim();
+            System.out.println("Найденная дата на сайте: " + dateText);
+
+            MonthDay eventMonthDay;
+            try {
+                eventMonthDay = MonthDay.parse(dateText, formatter);
+                System.out.println("Преобразованная дата для сравнения: " + eventMonthDay);
+            } catch (Exception e) {
+                throw new RuntimeException("Невозможно разобрать дату: " + dateText, e);
+            }
+
+            Assertions.assertFalse(eventMonthDay.isBefore(currentMonthDay),
+                    "Дата мероприятия " + eventMonthDay.format(formatter) + " меньше текущей даты.");
         }
         return this;
     }
